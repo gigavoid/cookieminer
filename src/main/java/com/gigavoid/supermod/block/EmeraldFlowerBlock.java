d0eb29593d323dc2a24acc23ce1e062fd1e18edc
Rasmus <steen.rasmus.95@gimail.com>
2014-10-25 00:37:01 +0200

    Fixat partiklar på de magiska blommorna

--- src/main/java/com/gigavoid/supermod/block/EmeraldFlowerBlock.java
+++ src/main/java/com/gigavoid/supermod/block/EmeraldFlowerBlock.java
@@ -16,8 +16,8 @@ public class EmeraldFlowerBlock extends BlockFlower {
     }
 
     @Override
-    public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random) {
-        super.updateTick(par1World, par2, par3, par4, par5Random);
-        par1World.spawnParticle("largeexplode", par2, par3, par4, 0.0D, 1.0D, 0.0D);
+    public void randomDisplayTick(World p_149734_1_, int p_149734_2_, int p_149734_3_, int p_149734_4_, Random p_149734_5_) {
+        super.randomDisplayTick(p_149734_1_, p_149734_2_, p_149734_3_, p_149734_4_, p_149734_5_);
+        p_149734_1_.spawnParticle("reddust", p_149734_2_ + 0.5, p_149734_3_ + 0.5, p_149734_4_ + 0.5, 0.0D, 25.0D, 0.0D);
     }
 }
