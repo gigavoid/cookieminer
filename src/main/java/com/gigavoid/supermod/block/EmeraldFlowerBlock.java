6d02ad62c3a4cca9737ade701e2cca9eb65478db
Rasmus <steen.rasmus.95@gimail.com>
2014-10-24 23:25:01 +0200

    fixade namn oc c tab

--- src/main/java/com/gigavoid/supermod/block/EmeraldFlowerBlock.java
+++ src/main/java/com/gigavoid/supermod/block/EmeraldFlowerBlock.java
@@ -1,10 +1,13 @@
 package com.gigavoid.supermod.block;
 
 import net.minecraft.block.BlockFlower;
+import net.minecraft.creativetab.CreativeTabs;
 
 public class EmeraldFlowerBlock extends BlockFlower {
     public EmeraldFlowerBlock(){
         super(0);
         this.setLightLevel(1.0f);
+        this.setCreativeTab(CreativeTabs.tabDecorations);
+        this.setBlockName("emeraldFlower");
     }
 }
