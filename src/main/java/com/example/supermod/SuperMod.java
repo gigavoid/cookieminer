1102b83e23499895f93b40e386bb1e18da098746
Rasmus <steen.rasmus.95@gimail.com>
2014-10-24 20:51:32 +0200

    Set names and versins ids

--- src/main/java/com/example/supermod/SuperMod.java
+++ src/main/java/com/example/supermod/SuperMod.java
@@ -1,6 +1,5 @@
-package com.example.examplemod;
+package com.example.supermod;
 
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -8,16 +7,12 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = SuperMod.MODID, version = SuperMod.VERSION)
 public class SuperMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "The Super Mod";
     public static final String VERSION = "1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
 
-
-        System.out.println("Hej!");
     }
 }
