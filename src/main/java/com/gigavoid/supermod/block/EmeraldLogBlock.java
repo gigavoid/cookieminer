d6d291e74081637429dbc7edb54d811336fa8765
Rasmus <steen.rasmus.95@gimail.com>
2014-10-24 22:53:13 +0200

    fixed emerald log

--- src/main/java/com/gigavoid/supermod/block/EmeraldLogBlock.java
+++ src/main/java/com/gigavoid/supermod/block/EmeraldLogBlock.java
@@ -1,17 +1,39 @@
 package com.gigavoid.supermod.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
 
 public class EmeraldLogBlock extends Block {
-    public EmeraldLogBlock(Material material){
-        super(material);
+
+    @SideOnly(Side.CLIENT)
+    private IIcon eLogIconTop;
+
+    public EmeraldLogBlock(){
+        super(Material.wood);
         this.setHardness(1.0f);
         this.setBlockTextureName("supermod:eLogSide");
         this.setHarvestLevel("axe", 0);
         this.setCreativeTab(CreativeTabs.tabBlock);
         this.setBlockName("emeraldLog");
         this.setStepSound(soundTypeWood);
+    }
+
+    @Override
+    public IIcon getIcon(int par1, int par2)
+    {
+        return par1 == 1 ? this.eLogIconTop : this.blockIcon;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon("supermod:eLogSide");
+        this.eLogIconTop = par1IconRegister.registerIcon("supermod:eLogBT");
     }
 }
--- src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
+++ src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
@@ -1,11 +1,10 @@
 package com.gigavoid.supermod.block;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.material.Material;
 
 public class SuperBlocks {
 
-    public static final EmeraldLogBlock emeraldLog = new EmeraldLogBlock(Material.wood);
+    public static final EmeraldLogBlock emeraldLog = new EmeraldLogBlock();
 
     public static void initializeBlocks(){
         GameRegistry.registerBlock(emeraldLog, "Emerald Log");
