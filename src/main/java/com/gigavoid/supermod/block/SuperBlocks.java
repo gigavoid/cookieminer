ab0fc123d44fa571e9793012151e21fce41a9240
Lukas Annsberg <lulleannsberg@gmail.com>
2014-10-24 22:43:34 +0200

    kolla mitt block

--- src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
+++ src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
@@ -6,8 +6,10 @@ import net.minecraft.block.material.Material;
 public class SuperBlocks {
 
     public static final EmeraldLogBlock emeraldLog = new EmeraldLogBlock(Material.wood);
+    public static final SuperStoneBlock superStone = new SuperStoneBlock();
 
     public static void initializeBlocks(){
         GameRegistry.registerBlock(emeraldLog, "Emerald Log");
+        GameRegistry.registerBlock(superStone, "Super Stone");
     }
 }
