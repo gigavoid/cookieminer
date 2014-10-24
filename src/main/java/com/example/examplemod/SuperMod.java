88eea735db1705cbde53998f92ada63e35609100
Rasmus <steen.rasmus.95@gimail.com>
2014-10-24 20:13:26 +0200

    Ändrade namn till SuperMod

---
+++ src/main/java/com/example/examplemod/SuperMod.java
@@ -0,0 +1,23 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SuperMod.MODID, version = SuperMod.VERSION)
+public class SuperMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+
+
+        System.out.println("Hej!");
+    }
+}
