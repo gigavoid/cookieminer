d45232e01aac1132bb2def6b57acd5087672fa12
Luguan <lulleannsberg@gmail.com>
2014-10-25 13:36:12 +0200

    fix?

--- src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
+++ src/main/java/com/gigavoid/supermod/block/SuperBlocks.java
@@ -4,14 +4,6 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class SuperBlocks {
 
-<<<<<<< HEAD
-    public static final EmeraldLogBlock emeraldLog = new EmeraldLogBlock(Material.wood);
-    public static final SuperStoneBlock superStone = new SuperStoneBlock();
-
-    public static void initializeBlocks(){
-        GameRegistry.registerBlock(emeraldLog, "Emerald Log");
-        GameRegistry.registerBlock(superStone, "Super Stone");
-=======
     public static final EmeraldLogBlock emeraldLog = new EmeraldLogBlock();
     public static final EmeraldFlowerBlock emeraldFlower = new EmeraldFlowerBlock();
     public static final OPBlock opblock = new OPBlock();
@@ -20,6 +12,5 @@ public class SuperBlocks {
         GameRegistry.registerBlock(emeraldLog, "emeraldLog");
         GameRegistry.registerBlock(emeraldFlower, "emeraldFlower");
         GameRegistry.registerBlock(opblock, "op");
->>>>>>> c69c24a82606151ad13f22662e3d70fdefb52ea2
     }
 }
