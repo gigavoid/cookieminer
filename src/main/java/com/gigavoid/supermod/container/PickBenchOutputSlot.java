b5308a2e1652f2b865a794bded0c1993e3e8a249
Henrik Karlsson <hk.henrik@gmail.com>
2014-10-27 00:06:21 +0100

    a

---
+++ src/main/java/com/gigavoid/supermod/container/PickBenchOutputSlot.java
@@ -0,0 +1,7 @@
+package com.gigavoid.supermod.container;
+
+/**
+ * Created by ineentho on 2014-10-26.
+ */
+public class PickBenchOutputSlot {
+}
--- src/main/java/com/gigavoid/supermod/progpick/ProgPickUpgrade.java
+++ src/main/java/com/gigavoid/supermod/progpick/ProgPickUpgrade.java
@@ -1,9 +1,27 @@
 package com.gigavoid.supermod.progpick;
 
 public class ProgPickUpgrade {
-
     public String name;
     public int reqLevel;
+
+    public String getDisplayLevel(int level) {
+        if(name == "Harvest Level") {
+            switch(level){
+                case 0:
+                    return "Coal";
+                case 1:
+                    return "Iron";
+                case 2:
+                    return "Diamond";
+                case 3:
+                    return "Obidian";
+                case 4:
+                    return "Over Powered";
+            }
+        }
+
+        return Integer.toString(level);
+    }
 
     public ProgPickUpgrade(String name) {
         this(name, 1);
