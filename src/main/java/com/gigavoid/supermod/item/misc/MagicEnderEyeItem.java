8b3d856c55e79ca8217417bf26bbc4a3c6fd320c
Rasmus <steen.rasmus.95@gimail.com>
2014-10-26 20:07:32 +0100

    asd

--- src/main/java/com/gigavoid/supermod/item/misc/MagicEnderEyeItem.java
+++ src/main/java/com/gigavoid/supermod/item/misc/MagicEnderEyeItem.java
@@ -1,30 +1,32 @@
-package com.gigavoid.supermod.item.material;
+package com.gigavoid.supermod.item.misc;
 
-import net.minecraft.block.BlockTNT;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 
-/**
- * Created by Rasmus on 2014-10-26.
- */
 public class MagicEnderEyeItem extends Item {
     public MagicEnderEyeItem(){
         super();
         maxStackSize = 16;
         setCreativeTab(CreativeTabs.tabMisc);
         setTextureName("supermod:magic_ender_eye");
-        setMaxDamage(1);
     }
 
     public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player)
     {
         if (player.getCurrentEquippedItem() != null && player.getCurrentEquippedItem().getItem() == this)
         {
-            world.createExplosion(null, (int)player.getPosition(0).xCoord, (int)player.getPosition(0).yCoord, (int)player.getPosition(0).zCoord, 4.0f, true);
+            createExplosion(world, null, player.getPosition(0).xCoord, player.getPosition(0).yCoord, player.getPosition(0).zCoord, 2.0f, true);
         }
         return new ItemStack(this, itemStack.stackSize - 1);
+    }
+
+    public Explosion createExplosion(World world, Entity p_72876_1_, double p_72876_2_, double p_72876_4_, double p_72876_6_, float p_72876_8_, boolean p_72876_9_)
+    {
+        return world.newExplosion(p_72876_1_, p_72876_2_, p_72876_4_, p_72876_6_, p_72876_8_, true, p_72876_9_);
     }
 }
