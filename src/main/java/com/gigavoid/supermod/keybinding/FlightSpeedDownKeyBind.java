26bc3c47a2c1d2bf20156b846c87c9f47980e9da
Henrik Karlsson <hk.henrik@gmail.com>
2014-11-26 22:46:59 +0100

    Skev autoflight fixad

--- src/main/java/com/gigavoid/supermod/keybinding/FlightSpeedDownKeyBind.java
+++ src/main/java/com/gigavoid/supermod/keybinding/FlightSpeedDownKeyBind.java
@@ -2,14 +2,12 @@ package com.gigavoid.supermod.keybinding;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
 
 /**
  * Created by ineentho on 2014-11-26.
  */
 public class FlightSpeedDownKeyBind extends SuperKeyBinding {
-    public static boolean autoFlying = false;
 
     public FlightSpeedDownKeyBind() {
         super("Flight speed down", Keyboard.KEY_SUBTRACT, "Super Mod");
@@ -17,8 +15,6 @@ public class FlightSpeedDownKeyBind extends SuperKeyBinding {
 
     @Override
     public void Pressed() {
-        autoFlying = !autoFlying;
-        KeyBinding.setKeyBindState(Keyboard.KEY_W, autoFlying);
         EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
         player.capabilities.setFlySpeed(player.capabilities.getFlySpeed() * .8f);
     }
