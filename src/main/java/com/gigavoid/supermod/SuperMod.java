b89213134e0a19f15796bb85269b299cc43a946b
Henrik Karlsson <hk.henrik@gmail.com>
2014-11-29 01:18:42 +0100

    Revert "Början till custom village"
    This reverts commit 5601d39c2bd9671615321594d65db061bf6f416a.

--- src/main/java/com/gigavoid/supermod/SuperMod.java
+++ src/main/java/com/gigavoid/supermod/SuperMod.java
@@ -14,7 +14,6 @@ import com.gigavoid.supermod.worldgen.SuperWorldGens;
 import com.gigavoid.supermod.worldgen.northrend.NorthrendVillageBlockReplacer;
 import com.gigavoid.supermod.worldgen.northrend.WorldProviderNorthrend;
 import com.gigavoid.supermod.worldgen.northrend.WorldTypeNorthrend;
-import com.gigavoid.supermod.worldgen.structure.SuperStructureVillagePieces;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -43,8 +42,6 @@ public class SuperMod
 
         DimensionManager.registerProviderType(northrendDimID, WorldProviderNorthrend.class, false);
         DimensionManager.registerDimension(northrendDimID, northrendDimID);
-
-        SuperStructureVillagePieces.registerStructures();
     }
 
     @EventHandler
