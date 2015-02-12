6a21d649ae6ff8c00a10a63bb0d0cb70b70b900d
Henrik Karlsson <hk.henrik@gmail.com>
2015-02-13 00:12:03 +0100

    Superrent mod

--- src/main/java/com/gigavoid/supermod/SuperMod.java
+++ src/main/java/com/gigavoid/supermod/SuperMod.java
@@ -1,20 +1,5 @@
 package com.gigavoid.supermod;
 
-import com.gigavoid.supermod.biome.SuperBiomes;
-import com.gigavoid.supermod.block.SuperBlocks;
-import com.gigavoid.supermod.entity.SuperEntities;
-import com.gigavoid.supermod.gui.SuperGuiHandler;
-import com.gigavoid.supermod.item.SuperItems;
-import com.gigavoid.supermod.keybinding.SuperKeyBinds;
-import com.gigavoid.supermod.recepies.SuperRecipie;
-import com.gigavoid.supermod.recepies.SuperSmeltingRecipie;
-import com.gigavoid.supermod.renderer.SuperRenderer;
-import com.gigavoid.supermod.tileentity.SuperTileEntities;
-import com.gigavoid.supermod.worldgen.SuperWorldGens;
-import com.gigavoid.supermod.worldgen.northrend.WorldProviderNorthrend;
-import com.gigavoid.supermod.worldgen.northrend.WorldTypeNorthrend;
-import net.minecraft.world.WorldType;
-import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -24,33 +9,16 @@ public class SuperMod
 {
     @Mod.Instance("supermod")
     public static SuperMod instance;
-
     public static final String MODID = "supermod";
     public static final String VERSION = "1.0";
 
-    public static final int northrendDimID = 2;
-    public static final WorldType northrend  = new WorldTypeNorthrend(7, "northrend");
-
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        SuperItems.initializeItems();
-        SuperBiomes.registerBiomes();
-
-        DimensionManager.registerProviderType(northrendDimID, WorldProviderNorthrend.class, false);
-        DimensionManager.registerDimension(northrendDimID, northrendDimID);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        SuperBlocks.initializeBlocks();
-        SuperRenderer.registerRenderers();
-        SuperWorldGens.initializeWorldGens();
-        SuperKeyBinds.registerKeybinds();
-        SuperGuiHandler.initializeGuis();
-        SuperRecipie.initializeRecipes();
-        SuperSmeltingRecipie.InitializeSmektingRecipes();
-        SuperEntities.registerEntities();
-        SuperTileEntities.initializeEntities();
+        System.out.println("Super Mod loaded");
     }
 }
