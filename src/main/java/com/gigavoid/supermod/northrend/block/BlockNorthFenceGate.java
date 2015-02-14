385bccbaa441c14c2fb42693da83925b8d31130b
Rasmus Steen <steen.rasmus.95@gmail.com>
2015-02-14 08:11:56 +0100

    Fixed fences still working on gates

--- src/main/java/com/gigavoid/supermod/northrend/block/BlockNorthFenceGate.java
+++ src/main/java/com/gigavoid/supermod/northrend/block/BlockNorthFenceGate.java
@@ -6,6 +6,6 @@ import net.minecraft.creativetab.CreativeTabs;
 public class BlockNorthFenceGate extends BlockFenceGate {
     public BlockNorthFenceGate(){
         super();
-        this.setCreativeTab(CreativeTabs.tabDecorations);
+        this.setCreativeTab(CreativeTabs.tabRedstone);
     }
 }
