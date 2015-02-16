b956b2392b7eeaacbb5f6969a0cf93d37e844b1e
Henrik Karlsson <hk.henrik@gmail.com>
2015-02-16 20:51:29 +0100

    Ropeway engine rotation and autopop

--- src/main/java/com/gigavoid/supermod/ropeway/block/BlockRopewayEngine.java
+++ src/main/java/com/gigavoid/supermod/ropeway/block/BlockRopewayEngine.java
@@ -6,14 +6,21 @@ import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Vec3i;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import java.awt.*;
+
 public class BlockRopewayEngine extends Block implements ITileEntityProvider {
+
 
     public static final PropertyDirection rotation = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
@@ -23,13 +30,46 @@ public class BlockRopewayEngine extends Block implements ITileEntityProvider {
     }
 
     @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
+    public boolean isVisuallyOpaque() {
+        return false;
+    }
+
+    @Override
+    public boolean canReplace(World worldIn, BlockPos pos, EnumFacing side, ItemStack stack) {
+        //TODO
+        return canPlaceBlockOnSide(worldIn, pos, side);
+    }
+
+    @Override
+    public boolean isTranslucent() {
+        return super.isTranslucent();
+    }
+
+    @Override
     public int getRenderType() {
         return -1;
     }
 
     @Override
-    public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) {
-        return true;
+    public boolean canPlaceBlockOnSide(World world, BlockPos pos, EnumFacing side) {
+        return placeOnSide(world, pos, side, new Vec3i(0, 0, 1), EnumFacing.NORTH) ||
+               placeOnSide(world, pos, side, new Vec3i(0, 0, -1), EnumFacing.SOUTH) ||
+               placeOnSide(world, pos, side, new Vec3i(1, 0, 0), EnumFacing.WEST) ||
+               placeOnSide(world, pos, side, new Vec3i(-1, 0, 0), EnumFacing.EAST);
+    }
+
+    private boolean placeOnSide(World world, BlockPos pos, EnumFacing side, Vec3i diff, EnumFacing direction) {
+        return side == direction && world.isSideSolid(pos.add(diff), direction) && world.getBlockState(pos.add(diff)).getBlock() == RopewayBlocks.pylon;
+    }
+
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, rotation);
     }
 
     @Override
@@ -38,7 +78,39 @@ public class BlockRopewayEngine extends Block implements ITileEntityProvider {
     }
 
     @Override
+    public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock) {
+        super.onNeighborBlockChange(worldIn, pos, state, neighborBlock);
+
+        EnumFacing direction = (EnumFacing) state.getValue(rotation);
+
+        switch (direction) {
+            case NORTH:
+                popIfNoPylon(worldIn, pos, pos.offsetSouth());
+                break;
+            case EAST:
+                popIfNoPylon(worldIn, pos, pos.offsetWest());
+                break;
+            case SOUTH:
+                popIfNoPylon(worldIn, pos, pos.offsetNorth());
+                break;
+            case WEST:
+                popIfNoPylon(worldIn, pos, pos.offsetEast());
+                break;
+        }
+    }
+
+    private void popIfNoPylon(World worldIn,BlockPos engine, BlockPos pylon) {
+        if(worldIn.getBlockState(pylon).getBlock() != RopewayBlocks.pylon)
+            worldIn.destroyBlock(engine, true);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing)state.getValue(rotation)).getIndex();
+    }
+
+    @Override
     public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
+        return this.getDefaultState().withProperty(rotation, facing);
     }
 }
