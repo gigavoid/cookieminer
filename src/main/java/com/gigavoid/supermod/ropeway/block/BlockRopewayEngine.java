efdafedb52153e69fec2c1fd0c05892732e00e7d
Henrik Karlsson <hk.henrik@gmail.com>
2015-02-16 19:03:35 +0100

    Ropeway engine without renderer

--- src/main/java/com/gigavoid/supermod/ropeway/block/BlockRopewayEngine.java
+++ src/main/java/com/gigavoid/supermod/ropeway/block/BlockRopewayEngine.java
@@ -1,11 +1,44 @@
 package com.gigavoid.supermod.ropeway.block;
 
+import com.gigavoid.supermod.ropeway.creativetab.RopewayCreativeTabs;
+import com.gigavoid.supermod.ropeway.tileentity.TileEntityRopewayEngine;
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
-public class BlockRopewayEngine extends Block {
+public class BlockRopewayEngine extends Block implements ITileEntityProvider {
+
+    public static final PropertyDirection rotation = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
     protected BlockRopewayEngine() {
         super(Material.rock);
+        this.setCreativeTab(RopewayCreativeTabs.tabBlock);
+    }
+
+    @Override
+    public int getRenderType() {
+        return -1;
+    }
+
+    @Override
+    public boolean canPlaceBlockOnSide(World worldIn, BlockPos pos, EnumFacing side) {
+        return true;
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new TileEntityRopewayEngine();
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return super.onBlockPlaced(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer);
     }
 }
