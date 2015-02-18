1465787dc8c76fb5b11405e2c27bff41762baf23
Henrik Karlsson <hk.henrik@gmail.com>
2015-02-18 21:35:52 +0100

    Ropeway stuff

--- src/main/java/com/gigavoid/supermod/ropeway/entity/EntityRopewayBasket.java
+++ src/main/java/com/gigavoid/supermod/ropeway/entity/EntityRopewayBasket.java
@@ -1,12 +1,16 @@
 package com.gigavoid.supermod.ropeway.entity;
 
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class EntityRopewayBasket extends Entity {
 
@@ -37,16 +41,16 @@ public class EntityRopewayBasket extends Entity {
     }
 
     @Override
-    public double getYOffset() {
-        return -4;
+    public double getMountedYOffset() {
+        return -.2;
     }
-
 
     @Override
     public boolean attackEntityFrom(DamageSource source, float amount) {
         this.setDead();
         return true;
     }
+
 
     @Override
     public boolean interactFirst(EntityPlayer playerIn)
@@ -64,11 +68,17 @@ public class EntityRopewayBasket extends Entity {
             if (!this.worldObj.isRemote)
             {
                 playerIn.mountEntity(this);
-                motionY = 500;
+                addVelocity(5, 5, 5);
             }
 
             return true;
         }
+    }
+
+    @Override
+    public void func_180426_a(double p_180426_1_, double p_180426_3_, double p_180426_5_, float p_180426_7_, float p_180426_8_, int p_180426_9_, boolean p_180426_10_) {
+        this.setPosition(p_180426_1_, p_180426_3_, p_180426_5_);
+        this.setRotation(p_180426_7_, p_180426_8_);
     }
 
     @Override
@@ -77,6 +87,13 @@ public class EntityRopewayBasket extends Entity {
         return entityIn.getEntityBoundingBox();
     }
 
+
+    @Override
+    public void onEntityUpdate() {
+        super.onEntityUpdate();
+        addVelocity(50, 50, 50);
+        motionY += 15;
+    }
 
     public EntityRopewayBasket(World world, double x, double y, double z) {
         super(world);
@@ -87,7 +104,7 @@ public class EntityRopewayBasket extends Entity {
         this.motionX = 0.0D;
         this.motionY = 0.0D;
         this.motionZ = 0.0D;
-        noClip = false;
+        noClip = true;
         setSize(1, .7f);
 
         if(!(x == 0 && y == 0 && z == 0)) {
