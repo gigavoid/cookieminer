932101459501d3839b6f5ebaa14a5edeea339176
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-20 23:24:45 +0100

    Kakkastaren är roterbar

--- src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookieCrafter.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookieCrafter.java
@@ -3,6 +3,7 @@ package com.gigavoid.supermod.cookiecraft.block;
 import com.gigavoid.supermod.cookiecraft.creativetab.CookiecraftCreativeTabs;
 import com.gigavoid.supermod.cookiecraft.tileentity.TileEntityCookieCrafter;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockPistonBase;
 import net.minecraft.block.BlockSourceImpl;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
@@ -12,6 +13,7 @@ import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.dispenser.BehaviorDefaultDispenseItem;
 import net.minecraft.dispenser.IBehaviorDispenseItem;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -31,6 +33,50 @@ public class BlockCookieCrafter extends Block implements ITileEntityProvider {
     public BlockCookieCrafter() {
         super(Material.rock);
         setCreativeTab(CookiecraftCreativeTabs.tabCookiecraft);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, placer.func_174811_aO().getOpposite());
+    }
+
+    /**
+     * Copied from BlockDispenser
+     */
+    private void setDefaultDirection(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (!worldIn.isRemote)
+        {
+            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+            boolean flag = worldIn.getBlockState(pos.offsetNorth()).getBlock().isFullBlock();
+            boolean flag1 = worldIn.getBlockState(pos.offsetSouth()).getBlock().isFullBlock();
+
+            if (enumfacing == EnumFacing.NORTH && flag && !flag1)
+            {
+                enumfacing = EnumFacing.SOUTH;
+            }
+            else if (enumfacing == EnumFacing.SOUTH && flag1 && !flag)
+            {
+                enumfacing = EnumFacing.NORTH;
+            }
+            else
+            {
+                boolean flag2 = worldIn.getBlockState(pos.offsetWest()).getBlock().isFullBlock();
+                boolean flag3 = worldIn.getBlockState(pos.offsetEast()).getBlock().isFullBlock();
+
+                if (enumfacing == EnumFacing.WEST && flag2 && !flag3)
+                {
+                    enumfacing = EnumFacing.EAST;
+                }
+                else if (enumfacing == EnumFacing.EAST && flag3 && !flag2)
+                {
+                    enumfacing = EnumFacing.WEST;
+                }
+            }
+
+            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+        }
     }
 
     @Override
@@ -38,6 +84,7 @@ public class BlockCookieCrafter extends Block implements ITileEntityProvider {
         if (!worldIn.isRemote) {
             worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn));
         }
+        this.setDefaultDirection(worldIn, pos, state);
     }
 
     @Override
@@ -49,7 +96,6 @@ public class BlockCookieCrafter extends Block implements ITileEntityProvider {
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
         if (!worldIn.isRemote)
         {
-            System.out.println("Kasta kaka!");
             throwCookie(worldIn, pos);
             worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn));
         }
