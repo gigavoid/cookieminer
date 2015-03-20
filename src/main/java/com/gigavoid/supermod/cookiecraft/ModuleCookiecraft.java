2245e7453bd06489a20d5d1bee2e3fa849d1da13
Rasmus Steen <steen.rasmus.95@gmail.com>
2015-03-20 21:21:57 +0100

    cookiecraft added

---
+++ src/main/java/com/gigavoid/supermod/cookiecraft/ModuleCookiecraft.java
@@ -0,0 +1,18 @@
+package com.gigavoid.supermod.cookiecraft;
+
+import com.gigavoid.supermod.common.module.Module;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ModuleCookiecraft extends Module{
+
+    @Override
+    public void preInit(FMLPreInitializationEvent e) {
+
+    }
+
+    @Override
+    public void init(FMLInitializationEvent e) {
+        
+    }
+}
