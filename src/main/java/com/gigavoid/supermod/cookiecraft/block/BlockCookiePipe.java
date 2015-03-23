07998e2acb7fadf459bdad01cc7b3af3d9d74223
Rasmus Steen <steen.rasmus.95@gmail.com>
2015-03-24 00:38:10 +0100

    bounding box for pipes

--- src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookiePipe.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookiePipe.java
@@ -6,10 +6,14 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BlockCookiePipe extends Block implements ICookieBlock {
     public static final PropertyBool NORTH = PropertyBool.create("north");
@@ -22,6 +26,145 @@ public class BlockCookiePipe extends Block implements ICookieBlock {
     public BlockCookiePipe(){
         super(Material.iron);
         this.setCreativeTab(CookiecraftCreativeTabs.tabCookiecraft);
+    }
+
+    public void addCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List list, Entity collidingEntity)
+    {
+        boolean north = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.NORTH);
+        boolean south = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.SOUTH);
+        boolean west = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.WEST);
+        boolean east = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.EAST);
+        boolean up = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.UP);
+        boolean down = this.isNeighborACookieBlock(worldIn, pos, EnumFacing.DOWN);
+        float xMin = 0.3125F;
+        float xMax = 0.6875F;
+        float zMin = 0.3125F;
+        float zMax = 0.6875F;
+        float yMin = 0.3125F;
+        float yMax = 0.6875F;
+
+        if (down)
+        {
+            yMin = 0.0F;
+        }
+
+        if (up)
+        {
+            yMax = 1.0F;
+        }
+
+        if (down || up)
+        {
+            this.setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
+            super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+        }
+
+        yMin = 0.3125F;
+        yMax = 0.6875F;
+
+        if (north)
+        {
+            zMin = 0.0F;
+        }
+
+        if (south)
+        {
+            zMax = 1.0F;
+        }
+
+        if (north || south || !down && !up)
+        {
+            this.setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
+            super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+        }
+
+        yMin = 0.3125F;
+        yMax = 0.6875F;
+        zMin = 0.3125F;
+        zMax = 0.6875F;
+
+        if (west)
+        {
+            xMin = 0.0F;
+        }
+
+        if (east)
+        {
+            xMax = 1.0F;
+        }
+
+        if (west || east || !north && !south || !down && !up)
+        {
+            this.setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
+            super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+        }
+
+        if (north)
+        {
+            zMin = 0.0F;
+        }
+
+        if (south)
+        {
+            zMax = 1.0F;
+        }
+        if (down)
+        {
+            yMin = 0.0F;
+        }
+        if (up)
+        {
+            yMax = 1.0F;
+        }
+
+        this.setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
+        super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+    }
+
+    public void setBlockBoundsBasedOnState(IBlockAccess access, BlockPos pos)
+    {
+        boolean north = this.isNeighborACookieBlock(access, pos, EnumFacing.NORTH);
+        boolean south = this.isNeighborACookieBlock(access, pos, EnumFacing.SOUTH);
+        boolean west = this.isNeighborACookieBlock(access, pos, EnumFacing.WEST);
+        boolean east = this.isNeighborACookieBlock(access, pos, EnumFacing.EAST);
+        boolean up = this.isNeighborACookieBlock(access, pos, EnumFacing.UP);
+        boolean down = this.isNeighborACookieBlock(access, pos, EnumFacing.DOWN);
+        float xMin = 0.3125F;
+        float xMax = 0.6875F;
+        float zMin = 0.3125F;
+        float zMax = 0.6875F;
+        float yMin = 0.3125F;
+        float yMax = 0.6875F;
+
+        if (north)
+        {
+            zMin = 0.0F;
+        }
+
+        if (south)
+        {
+            zMax = 1.0F;
+        }
+
+        if (west)
+        {
+            xMin = 0.0F;
+        }
+
+        if (east)
+        {
+            xMin = 1.0F;
+        }
+        if (down)
+        {
+            yMin = 0.0F;
+        }
+        if (up)
+        {
+            yMax = 1.0F;
+        }
+
+        this.setBlockBounds(xMin, yMin, zMin, xMax, yMax, zMax);
     }
 
     @Override
