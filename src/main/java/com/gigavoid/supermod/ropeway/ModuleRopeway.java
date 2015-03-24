7f50f7fb734cbdb440c69db52cf364dc0b19afa3
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-24 15:53:20 +0100

    Not registering rendering stuff on the server

--- src/main/java/com/gigavoid/supermod/ropeway/ModuleRopeway.java
+++ src/main/java/com/gigavoid/supermod/ropeway/ModuleRopeway.java
@@ -5,6 +5,7 @@ import com.gigavoid.supermod.ropeway.biome.RopewayBiomes;
 import com.gigavoid.supermod.ropeway.entity.RopewayEntities;
 import com.gigavoid.supermod.ropeway.item.RopewayItems;
 import com.gigavoid.supermod.ropeway.block.RopewayBlocks;
+import com.gigavoid.supermod.ropeway.renderer.RendererRopewayEngine;
 import com.gigavoid.supermod.ropeway.tileentity.TileEntityRopewayEngine;
 import com.gigavoid.supermod.ropeway.worldgen.RopewayWorldProvider;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -12,13 +13,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ModuleRopeway extends Module {
     public static int dimensionId;
 
     @Override
     public void preInit(FMLPreInitializationEvent e) {
-        RopewayBiomes.registerBiomes(getRegister(e.getSide()));
+	   	RopewayBiomes.registerBiomes(getRegister(e.getSide()));
 
         dimensionId = getRegister(e.getSide()).registerDimension(RopewayWorldProvider.class, false);
     }
@@ -27,11 +29,17 @@ public class ModuleRopeway extends Module {
     public void init(FMLInitializationEvent e) {
         RopewayBlocks.initializeBlocks(getRegister(e.getSide()));
         RopewayItems.initializeItems(getRegister(e.getSide()), e);
-        RopewayEntities.registerEntities();
+        RopewayEntities.registerEntities(e.getSide());
 
         GameRegistry.registerTileEntity(TileEntityRopewayEngine.class, "ropeway_engine");
 
-		if (e.getSide() == Side.CLIENT)
-        	ClientRegistry.bindTileEntitySpecialRenderer(TileEntityRopewayEngine.class, new com.gigavoid.supermod.ropeway.renderer.RendererRopewayEngine());
-    }
+		if (e.getSide() == Side.CLIENT) {
+			registerTileEntity();
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void registerTileEntity() {
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityRopewayEngine.class, new RendererRopewayEngine());
+	}
 }
--- src/main/java/com/gigavoid/supermod/ropeway/entity/RopewayEntities.java
+++ src/main/java/com/gigavoid/supermod/ropeway/entity/RopewayEntities.java
@@ -5,19 +5,35 @@ import com.gigavoid.supermod.ropeway.renderer.RendererRopewayBasket;
 import com.gigavoid.supermod.ropeway.renderer.RendererRope;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class RopewayEntities {
-    public static void registerEntities(){
-        registerWithRenderer(EntityRope.class, new RendererRope(Minecraft.getMinecraft().getRenderManager()), "entity_rope", 64, -1, 5);
-        registerWithRenderer(EntityRopewayBasket.class, new RendererRopewayBasket(Minecraft.getMinecraft().getRenderManager()), "entity_basket", 64, 1, 6);
+    public static void registerEntities(Side side){
 
+		registerEntity(EntityRope.class, "entity_rope", 64, -1, 5);
+        registerEntity(EntityRopewayBasket.class, "entity_basket", 64, 1, 6);
+
+		if (side == Side.CLIENT)
+			registerRenderers();
     }
 
-    private static void registerWithRenderer(Class clazz, Render renderer, String name, int range, int freq, int id) {
+	@SideOnly(Side.CLIENT)
+	private static void registerRenderers() {
+		registerRenderer(EntityRope.class, new RendererRope(Minecraft.getMinecraft().getRenderManager()));
+		registerRenderer(EntityRopewayBasket.class, new RendererRopewayBasket(Minecraft.getMinecraft().getRenderManager()));
+	}
+
+	@SideOnly(Side.CLIENT)
+	private static void registerRenderer(Class<? extends Entity> clazz, Render renderer) {
+		RenderingRegistry.registerEntityRenderingHandler(clazz, renderer);
+	}
+
+	private static void registerEntity(Class clazz, String name, int range, int freq, int id) {
         EntityRegistry.registerModEntity(clazz, name, id, SuperMod.instance, range, freq, true);
         EntityRegistry.registerGlobalEntityID(clazz, name, id);
-        RenderingRegistry.registerEntityRenderingHandler(clazz, renderer);
     }
 }
