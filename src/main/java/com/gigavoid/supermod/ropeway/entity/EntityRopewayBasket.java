89962ca5f1d0f5c5188ef659fbe552d1cccb672e
Henrik Karlsson <hk.henrik@gmail.com>
2015-03-24 18:31:01 +0100

    A start of smoothing out movement

--- src/main/java/com/gigavoid/supermod/ropeway/entity/EntityRopewayBasket.java
+++ src/main/java/com/gigavoid/supermod/ropeway/entity/EntityRopewayBasket.java
@@ -29,8 +29,15 @@ public class EntityRopewayBasket extends Entity{
     private BlockPos target;
     private BlockPos prevTarget;
     private Random r = new Random();
-    
-    
+	public double smoothX = 0;
+	public double smoothY = 0;
+	public double smoothZ = 0;
+
+	public double lastSmoothX = 0;
+	public double lastSmoothY = 0;
+	public double lastSmoothZ = 0;
+
+
 
     public EntityRopewayBasket(World world) {
         this(world, 0, 0, 0);
@@ -40,7 +47,7 @@ public class EntityRopewayBasket extends Entity{
     public void setTarget(BlockPos target) {
         this.target = target;
 
-        if (!worldObj.isRemote) {
+        if (!worldObj.isRemote && target != null) {
             dataWatcher.updateObject(20, target.getX());
             dataWatcher.updateObject(21, target.getY());
             dataWatcher.updateObject(22, target.getZ());
@@ -182,10 +189,23 @@ public class EntityRopewayBasket extends Entity{
             Vec3 diff = new Vec3(xDiff, yDiff, zDiff);
             diff = diff.normalize();
 
-            posX += diff.xCoord * SPEED;
-            posY += diff.yCoord * SPEED;
-            posZ += diff.zCoord * SPEED;
+
+			posX += diff.xCoord * SPEED;
+			posY += diff.yCoord * SPEED;
+			posZ += diff.zCoord * SPEED;
         }
+
+		if (worldObj.isRemote) {
+			lastSmoothX = smoothX;
+			lastSmoothY = smoothY;
+			lastSmoothZ = smoothZ;
+
+			/*smoothX += (posX - smoothX) * 1d;
+			smoothY += (posY - smoothY) * 1d;
+			smoothZ += (posZ - smoothZ) * 1d;*/
+
+			smoothX += 0;
+		}
     }
 
     private void findNewTarget() {
--- src/main/java/com/gigavoid/supermod/ropeway/renderer/RendererRopewayBasket.java
+++ src/main/java/com/gigavoid/supermod/ropeway/renderer/RendererRopewayBasket.java
@@ -1,7 +1,10 @@
 package com.gigavoid.supermod.ropeway.renderer;
 
+import com.gigavoid.supermod.ropeway.entity.EntityRopewayBasket;
 import com.gigavoid.supermod.ropeway.model.ModelRopewayBasket;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
@@ -12,32 +15,89 @@ import org.lwjgl.opengl.GL11;
 public class RendererRopewayBasket extends Render {
     public ModelRopewayBasket model = new ModelRopewayBasket();
 
+	private double renderPosX;
+	private double renderPosY;
+	private double renderPosZ;
+
     public RendererRopewayBasket(RenderManager p_i46179_1_) {
         super(p_i46179_1_);
     }
 
-    @Override
-    public void doRender(Entity entity, double x, double y, double z, float p_76986_8_, float tickProgress) {
-        GL11.glPushMatrix();
-        //GL11.glTranslatef((float) x + 0.5f, (float) y +.5f, (float) z + 0.5f);
+	private void calcRenderPos(Entity p_147936_1_, float smth, double posX, double posY, double posZ)
+	{
+		if (p_147936_1_.ticksExisted == 0)
+		{
+			p_147936_1_.lastTickPosX = p_147936_1_.posX;
+			p_147936_1_.lastTickPosY = p_147936_1_.posY;
+			p_147936_1_.lastTickPosZ = p_147936_1_.posZ;
+		}
 
-        x += (entity.posX - entity.lastTickPosX) * tickProgress;
-        y += (entity.posY - entity.lastTickPosY) * tickProgress;
-        z += (entity.posZ - entity.lastTickPosZ) * tickProgress;
-
-        GL11.glTranslated((float)x, (float)y, (float)z);
-
-        ResourceLocation textures = (new ResourceLocation("supermod:textures/blocks/ropeway_engine.png"));
-        Minecraft.getMinecraft().renderEngine.bindTexture(textures);
+		double d0 = p_147936_1_.lastTickPosX + (p_147936_1_.posX - p_147936_1_.lastTickPosX) * (double)smth;
+		double d1 = p_147936_1_.lastTickPosY + (p_147936_1_.posY - p_147936_1_.lastTickPosY) * (double)smth;
+		double d2 = p_147936_1_.lastTickPosZ + (p_147936_1_.posZ - p_147936_1_.lastTickPosZ) * (double)smth;
+		float f1 = p_147936_1_.prevRotationYaw + (p_147936_1_.rotationYaw - p_147936_1_.prevRotationYaw) * smth;
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
 
-        GL11.glPushMatrix();
-        GL11.glRotated(180, 1, 0,0);
-        model.render();
-        GL11.glPopMatrix();
-        GL11.glPopMatrix();
-    }
+		renderPosX = (posX - d0) * -1;
+		renderPosY = (posY - d1) * -1;
+		renderPosZ = (posZ - d2) * -1;
+	}
 
+	public void calcPos(EntityRopewayBasket entity, float tickProgress)
+	{
+		if (entity.ticksExisted == 0)
+		{
+			entity.lastSmoothX = entity.smoothX;
+			entity.lastSmoothY = entity.smoothY;
+			entity.lastSmoothZ = entity.smoothZ;
+		}
+
+		double d0 = entity.lastSmoothX + (entity.smoothX - entity.lastSmoothX) * (double)tickProgress;
+		double d1 = entity.lastSmoothY + (entity.smoothY - entity.lastSmoothY) * (double)tickProgress;
+		double d2 = entity.lastSmoothZ + (entity.smoothZ - entity.lastSmoothZ) * (double)tickProgress;
+		float f1 = entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * tickProgress;
+		int i = entity.getBrightnessForRender(tickProgress);
+
+		if (entity.isBurning())
+		{
+			i = 15728880;
+		}
+
+		int j = i % 65536;
+		int k = i / 65536;
+		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float) j / 1.0F, (float) k / 1.0F);
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		this.realRender(entity, d0 - this.renderPosX, d1 - this.renderPosY, d2 - this.renderPosZ, f1, tickProgress);
+	}
+
+	private void realRender(EntityRopewayBasket ropeway, double x, double y, double z, float f1, float tickProgress) {
+//		x += (ropeway.smoothX - ropeway.lastSmoothX) * tickProgress;
+//		y += (ropeway.smoothY - ropeway.lastSmoothY) * tickProgress;
+//		z += (ropeway.smoothZ - ropeway.lastSmoothZ) * tickProgress;
+
+		GL11.glTranslated((float)x, (float)y, (float)z);
+
+		ResourceLocation textures = (new ResourceLocation("supermod:textures/blocks/ropeway_engine.png"));
+		Minecraft.getMinecraft().renderEngine.bindTexture(textures);
+
+
+		GL11.glPushMatrix();
+		GL11.glRotated(180, 1, 0,0);
+		model.render();
+		GL11.glPopMatrix();
+		GL11.glPopMatrix();
+	}
+
+
+	@Override
+    public void doRender(Entity entity, double x, double y, double z, float smth, float tickProgress) {
+		GL11.glPushMatrix();
+
+		EntityRopewayBasket ropeway = (EntityRopewayBasket) entity;
+		calcRenderPos(ropeway, smth, x, y, z);
+		calcPos(ropeway, tickProgress);
+	}
     @Override
     protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
         return null;
