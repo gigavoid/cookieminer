671b62cfc188426ba26d34575d7d51193958e8d7
Rasmus Steen <steen.rasmus.95@gmail.com>
2015-04-28 22:56:43 +0200

    fdioeh

--- src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookieMoonlightReflector.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookieMoonlightReflector.java
@@ -5,6 +5,7 @@ import com.gigavoid.supermod.cookiecraft.gui.GuiCookieUpgrade;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
@@ -16,10 +17,22 @@ public class BlockCookieMoonlightReflector extends BlockCookieUpgradeBase implem
 
     @Override
     public double getCPS(World world, BlockPos pos, IBlockState state) {
-        if (!world.isDaytime() && world.canBlockSeeSky(pos)){
+        if (!world.isDaytime() && isTopBlock(world, pos)){
             return 1/15d;
         }
         return 0;
+    }
+
+    private boolean isTopBlock(World world, BlockPos pos){
+        boolean result = true;
+        int height = pos.getY();
+        while(result && height <= 255){
+            if (world.getBlockState(new BlockPos(pos.getX(), height, pos.getZ())).getBlock() == Blocks.air){
+                result = false;
+            }
+            height++;
+        }
+        return result;
     }
 
 	@Override
