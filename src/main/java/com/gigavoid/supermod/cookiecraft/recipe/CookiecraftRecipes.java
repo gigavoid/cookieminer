feec55ba7b6b8fb7d78e81e0eae2c050e6dcb2f9
Henrik Karlsson <hk.henrik@gmail.com>
2015-08-20 13:28:34 +0200

    Crafting recipes containing cookies boxes

--- src/main/java/com/gigavoid/supermod/cookiecraft/recipe/CookiecraftRecipes.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/recipe/CookiecraftRecipes.java
@@ -1,17 +1,11 @@
 package com.gigavoid.supermod.cookiecraft.recipe;
 
 import com.gigavoid.supermod.cookiecraft.block.CookiecraftBlocks;
-import com.gigavoid.supermod.cookiecraft.cookie.CookieStorageItem;
 import com.gigavoid.supermod.cookiecraft.item.CookiecraftItems;
-import com.gigavoid.supermod.cookiecraft.item.ICookieStorageItem;
 import net.minecraft.init.Blocks;
-import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.init.Items;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CookiecraftRecipes {
@@ -19,7 +13,7 @@ public class CookiecraftRecipes {
         cookieCrafter();
         cookieDoughCollector();
         cookieDough();
-		lavaConverter();
+		cookieFarm();
     }
 
     private static void cookieCrafter(){
@@ -49,61 +43,15 @@ public class CookiecraftRecipes {
                 'w', wheat, 'c', cocoaBeans, 'm', milk);
     }
 
-	private static void lavaConverter() {
-		GameRegistry.addRecipe(new IRecipe() {
-			@Override
-			public boolean matches(InventoryCrafting ic, World worldIn) {
-				return isItem(0, ic, Item.getItemFromBlock(Blocks.stone)) &&
-						ic.getStackInSlot(1) == null &&
-						isItem(2, ic, Item.getItemFromBlock(Blocks.stone)) &&
-						ic.getStackInSlot(3) == null &&
-						isCookieStorage(100, ic.getStackInSlot(4)) &&
-						ic.getStackInSlot(5) == null &&
-						isItem(6, ic, Item.getItemFromBlock(Blocks.stone)) &&
-						ic.getStackInSlot(7) == null &&
-						isItem(8, ic, Item.getItemFromBlock(Blocks.stone));
-			}
+	private static void cookieFarm() {
+		ItemStack iron = new ItemStack(Items.iron_ingot);
+		ItemStack seed = new ItemStack(Items.wheat_seeds);
+		ItemStack cookieBox = new ItemStack(CookiecraftItems.cookieBox);
+		ItemStack dirt = new ItemStack(Item.getItemFromBlock(Blocks.dirt));
 
-			private boolean isItem(int slot, InventoryCrafting ic, Item item) {
-				return ic.getStackInSlot(slot) != null && ic.getStackInSlot(slot).getItem() == item;
-			}
 
-			private boolean isCookieStorage(int numCookies, ItemStack stackInSlot) {
-				if (stackInSlot != null && stackInSlot.getItem() instanceof  ICookieStorageItem) {
-					CookieStorageItem storage = new CookieStorageItem(stackInSlot);
-					return storage.getCookies() >= numCookies;
-				}
-				return false;
-			}
-
-			@Override
-			public ItemStack getCraftingResult(InventoryCrafting ic) {
-				return new ItemStack(CookiecraftBlocks.lavaConverter );
-			}
-
-			@Override
-			public int getRecipeSize() {
-				return 9;
-			}
-
-			@Override
-			public ItemStack getRecipeOutput() {
-				return null;
-			}
-
-			@Override
-			public ItemStack[] func_179532_b(InventoryCrafting inv) {
-				ItemStack stackInSlot = inv.getStackInSlot(4);
-				CookieStorageItem storage = new CookieStorageItem(stackInSlot);
-				storage.takeCookies(100);
-				ItemStack[] ret = new ItemStack[inv.getSizeInventory()];
-				for (int i = 0; i < ret.length; i++)
-				{
-					ret[i] = null;
-				}
-				ret[4] = stackInSlot;
-				return ret;
-			}
-		});
+		GameRegistry.addRecipe(new ShapedCookieRecepie(8, "isi", "scs", "idi",
+				'i', iron, 's', seed, 'c', cookieBox, 'd', dirt));
 	}
+
 }
---
+++ src/main/java/com/gigavoid/supermod/cookiecraft/recipe/ShapedCookieRecepie.java
@@ -0,0 +1,122 @@
+package com.gigavoid.supermod.cookiecraft.recipe;
+
+import com.gigavoid.supermod.cookiecraft.block.CookiecraftBlocks;
+import com.gigavoid.supermod.cookiecraft.cookie.CookieStorageItem;
+import com.gigavoid.supermod.cookiecraft.item.ICookieStorageItem;
+import com.gigavoid.supermod.cookiecraft.item.ItemCookieBoxBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShapedCookieRecepie implements IRecipe{
+    private final int cookiesRequired;
+    private List<ItemStack> recipe;
+
+    public ShapedCookieRecepie(int cookiesRequired, String shape1, String shape2, String shape3, Object ... args) {
+        this.cookiesRequired = cookiesRequired;
+
+        Map<String, ItemStack> letterMap = new HashMap<String, ItemStack>();
+
+        String key = null;
+        for(Object o : args) {
+            if (key == null) {
+                key = String.valueOf(o);
+            } else {
+                letterMap.put(key, (ItemStack) o);
+                key = null;
+            }
+        }
+
+        recipe = new ArrayList<ItemStack>();
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                String line;
+                if (row == 0)
+                    line = shape1;
+                else if (row == 1)
+                    line = shape2;
+                else
+                    line = shape3;
+
+                String ch = String.valueOf(line.charAt(col));
+                recipe.add(letterMap.get(ch));
+            }
+        }
+    }
+
+    @Override
+    public boolean matches(InventoryCrafting ic, World worldIn) {
+        for (int i = 0; i < recipe.size(); i++) {
+            ItemStack itemStack = recipe.get(i);
+            if (itemStack == null) {
+                if (ic.getStackInSlot(i) != null) {
+                    // There was an item in a slot that should be empty
+                    return false;
+                }
+            }
+            else if (itemStack.getItem() instanceof ItemCookieBoxBase) {
+                if (!isCookieStorage(cookiesRequired, ic.getStackInSlot(i))) {
+                    // Not enough cookies!
+                    return false;
+                }
+            }
+            else {
+                if (!isItem(i, ic, itemStack.getItem())) {
+                    // Wrong item was in the slot
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting ic) {
+        return new ItemStack(CookiecraftBlocks.lavaConverter );
+    }
+
+    @Override
+    public int getRecipeSize() {
+        return 9;
+    }
+
+    @Override
+    public ItemStack getRecipeOutput() {
+        return null;
+    }
+
+    @Override
+    public ItemStack[] func_179532_b(InventoryCrafting inv) {
+        ItemStack stackInSlot = inv.getStackInSlot(4);
+        CookieStorageItem storage = new CookieStorageItem(stackInSlot);
+        storage.takeCookies(cookiesRequired);
+        ItemStack[] ret = new ItemStack[inv.getSizeInventory()];
+        for (int i = 0; i < ret.length; i++)
+        {
+            ret[i] = null;
+        }
+        ret[4] = stackInSlot;
+        return ret;
+    }
+
+    private boolean isItem(int slot, InventoryCrafting ic, Item item) {
+        return ic.getStackInSlot(slot) != null && ic.getStackInSlot(slot).getItem() == item;
+    }
+
+    private boolean isCookieStorage(int numCookies, ItemStack stackInSlot) {
+        if (stackInSlot != null && stackInSlot.getItem() instanceof ICookieStorageItem) {
+            CookieStorageItem storage = new CookieStorageItem(stackInSlot);
+            return storage.getCookies() >= numCookies;
+        }
+        return false;
+    }
+
+}
