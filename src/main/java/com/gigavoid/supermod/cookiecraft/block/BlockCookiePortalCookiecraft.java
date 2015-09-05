909152606f9a8b91ab1d9145a8e1858897488c73
Rasmus Steen <steen.rasmus.95@gmail.com>
2015-09-05 23:22:59 +0200

    cookie portal bug fixed

--- src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookiePortalCookiecraft.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/block/BlockCookiePortalCookiecraft.java
@@ -343,7 +343,7 @@ public class BlockCookiePortalCookiecraft extends BlockBreakable {
         }
 
         protected boolean func_150857_a(Block p_150857_1_) {
-            Material material = (Material) Reflection.getFieldValue("blockMaterial", Block.class, p_150857_1_);
+            Material material = p_150857_1_.getMaterial();
             return material == Material.air || p_150857_1_ == CookiecraftBlocks.activator || p_150857_1_ == CookiecraftBlocks.portalCookiecraft;
         }
 
