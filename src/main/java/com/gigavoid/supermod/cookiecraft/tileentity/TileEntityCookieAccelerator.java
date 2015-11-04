e4c406a16c0dd4d803ecb168234e8c355636092d
Henrik Karlsson <hk.henrik@gmail.com>
2015-11-05 00:33:55 +0100

    Mark block for update

--- src/main/java/com/gigavoid/supermod/cookiecraft/tileentity/TileEntityCookieAccelerator.java
+++ src/main/java/com/gigavoid/supermod/cookiecraft/tileentity/TileEntityCookieAccelerator.java
@@ -1,6 +1,9 @@
 package com.gigavoid.supermod.cookiecraft.tileentity;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 
@@ -15,6 +18,7 @@ public class TileEntityCookieAccelerator extends TileEntity implements IUpdatePl
 
     public void setIsActive(boolean isActive) {
         this.isActive = isActive;
+        worldObj.markBlockForUpdate(pos);
     }
 
     @Override
@@ -32,5 +36,19 @@ public class TileEntityCookieAccelerator extends TileEntity implements IUpdatePl
     @Override
     public void update() {
 
+    }
+
+
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound compound = new NBTTagCompound();
+        compound.setBoolean(KEY_IS_ACTIVE, isActive);
+        return new S35PacketUpdateTileEntity(pos, 1, compound);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+        NBTTagCompound compound = pkt.getNbtCompound();
+        isActive = compound.getBoolean(KEY_IS_ACTIVE);
     }
 }
